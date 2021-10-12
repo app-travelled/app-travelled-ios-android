8bf799ed5bf81b767bae2404314ebe1434f197b1
Skesky <77647817+Skesky@users.noreply.github.com>
2021-10-13 00:15:20 +0200

    Bottoni riordinati e prima prova terra

--- Travelled/Travelled.Android/obj/Debug/110/android/environment.x86.s
+++ Travelled/Travelled.Android/obj/Debug/110/android/environment.x86.s
@@ -71,7 +71,7 @@ mono_aot_mode_name:
 	.section	.rodata.env.str.8,"aMS",@progbits,1
 	.type	.L.env.str.8, @object
 .L.env.str.8:
-	.asciz	"be8a98eb-aef7-4619-a8c6-dc54f9c1ceb4"
+	.asciz	"39b04856-621b-467f-a68c-c6eb92062a2e"
 	.size	.L.env.str.8, 37
 	.section	.rodata.env.str.9,"aMS",@progbits,1
 	.type	.L.env.str.9, @object
