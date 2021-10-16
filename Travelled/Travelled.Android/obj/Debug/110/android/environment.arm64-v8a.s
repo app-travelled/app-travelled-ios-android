13522482c7dcf7ee6effd9720abc0c2c0270e162
Davide Tedesco <davide_hrd@hotmail.it>
2021-10-16 22:54:27 +0200

    cambiati bottoni

--- Travelled/Travelled.Android/obj/Debug/110/android/environment.arm64-v8a.s
+++ Travelled/Travelled.Android/obj/Debug/110/android/environment.arm64-v8a.s
@@ -73,7 +73,7 @@ mono_aot_mode_name:
 	.section	.rodata.env.str.8,"aMS",@progbits,1
 	.type	.L.env.str.8, @object
 .L.env.str.8:
-	.asciz	"e750fa30-754e-4137-a982-3579f6f3e42c"
+	.asciz	"ed7ab52f-4f83-4641-8001-03f58bab7d1e"
 	.size	.L.env.str.8, 37
 	.section	.rodata.env.str.9,"aMS",@progbits,1
 	.type	.L.env.str.9, @object
