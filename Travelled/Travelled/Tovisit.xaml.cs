f4c957b7648f95793edbff66ecd2eba940725e7c
Davide Tedesco <davide_hrd@hotmail.it>
2021-10-29 11:23:59 +0200

    aggiunti bottoni share e credits
    Aggiunti bottoni e conseguenti azioni nella pagina ToVisit

--- Travelled/Travelled/Tovisit.xaml.cs
+++ Travelled/Travelled/Tovisit.xaml.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using Plugin.Share;
+using System.Diagnostics;
 
 namespace Travelled
 {
@@ -16,5 +18,20 @@ namespace Travelled
         {
             InitializeComponent();
         }
+        private void Share_Button_Clicked(object sender, EventArgs e)
+        {
+            CrossShare.Current.OpenBrowser("https://github.com/app-travelled/app-travelled-ios-android");
+        }
+
+        private void Credits_Button_Clicked(object sender, EventArgs e)
+        {
+            CrossShare.Current.OpenBrowser("https://github.com/app-travelled/app-travelled-ios-android");
+        }
+        async void OnActionSheetSimpleClicked(object sender, EventArgs e)
+        {
+            string action = await DisplayActionSheet("ActionSheet: Send to?", "Cancel", null, "Email", "Twitter", "Facebook");
+            Debug.WriteLine("Action: " + action);
+        }
     }
+
 }
